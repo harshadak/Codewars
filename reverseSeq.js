12368543f94b6bf2051f900ed5d68a0c092f8154
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2018-01-04 18:50:11 -0800

    Reverse Sequence

---
+++ reverseSeq.js
@@ -0,0 +1,12 @@
+// Reversed Sequence
+// Get the number n to return the reversed sequence from n to 1.
+
+const reverseSeq = n => {
+  var numSeq = [];
+  for (let i = n; i > 0; i--) {
+    numSeq.push(i);
+  }
+  return numSeq;
+};
+
+reverseSeq(-5);
