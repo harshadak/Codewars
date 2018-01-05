bd1934136d1e382c150672d1c69e97cc49cc33af
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2018-01-05 15:38:58 -0800

    Define Suit

---
+++ defineSuit.js
@@ -0,0 +1,15 @@
+// Define a card suit
+// You get any card as an argument. Your task is to return a suit of this card.
+
+function defineSuit(card) {
+  var cardLen = card.length - 1;
+  if (card[cardLen] == '♣') {
+    return "clubs";
+  } else if (card[cardLen] == '♦') {
+    return "diamonds";
+  } else if (card[cardLen] == '♥') {
+    return "hearts";
+  } else {
+    return "spades";
+  }
+}
